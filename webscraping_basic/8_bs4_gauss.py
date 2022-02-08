884950738d4d20377958a49f1364ff4b7d71c88d
Hyeongyeong Kim <vetibeti@naver.com>
2022-02-08 11:10:41 +0900

    fifth commit_webscraping

--- webscraping_basic/8_bs4_gauss.py
+++ webscraping_basic/8_bs4_gauss.py
@@ -9,13 +9,24 @@ soup = BeautifulSoup(res.text, "lxml")
 
 # 윗 부분 웹 스크래핑의 프레임 같은 부분
 
-cartoons = soup.find_all("td" , attrs={"class":"title"})
+#cartoons = soup.find_all("td" , attrs={"class":"title"})
 #title = cartoons[0].a.get_text()
 #link = cartoons[0].a["href"]
 #print(title)
 #print("https://comic.naver.com" + link)
 
+# 만화 제목 + 링크 가져오기 
+#for cartoon in cartoons:
+#    title = cartoon.a.get_text()
+#    link = "https://comic.naver.com" + cartoon.a["href"]
+#    print(title, link)    
+
+# 평점 구하기
+total_rates = 0
+cartoons = soup.find_all("div", attrs={"class":"rating_type"})
 for cartoon in cartoons:
-    title = cartoon.a.get_text()
-    link = "https://comic.naver.com" + cartoon.a["href"]
-    print(title, link)    
+    rate = cartoon.find("strong").get_text()
+    print(rate)
+    total_rates += float(rate)
+print("전체 점수 : " , total_rates)
+print("평균 점수 : " , total_rates / len(cartoons))
